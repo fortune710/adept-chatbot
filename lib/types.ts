4958edf7e72ed8bd99d6e18f2077771f608e003b
fortune710 <fortune0208@yahoo.com>
2024-10-17 15:50:08 -0400

    setup prisma and backend server

---
+++ lib/types.ts
@@ -0,0 +1,22 @@
+export interface Opportunity {
+  title: string;
+  description: string;
+  posteddate: Date;
+  archivedate: Date;
+  naicscode: string;
+  typeofsetaside: string;
+  typeofsetasidedescription: string;
+  noticeid: string;
+  solicitationnumber: string;
+  fullparentpathname: string;
+  fullparentpathcode: string;
+  type_op: string;
+  archivetype: string;
+  classificationcode: string;
+  pointofcontactname: string;
+  pointofcontactemail: string;
+  placeofperformancestate: string;
+  placeofperformancezip: string;
+  placeofperformancecountry: string;
+  ulink: string;
+}

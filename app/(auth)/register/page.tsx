24e19641b4a68f1d7e850e3eeea197d91e609506
Fortune Oluwasemilore Alebiosu <fortune0208@yahoo.com>
2024-10-22 16:57:16 -0400

    auth pages

---
+++ app/(auth)/register/page.tsx
@@ -0,0 +1,42 @@
+//import Image from "next/image";
+import AuthForm from "@/components/auth-form";
+import Link from "next/link";
+
+
+export default function RegisterPage() {
+  return (
+    <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
+      <div className="flex items-center justify-center py-12">
+        <div className="mx-auto grid w-[350px] gap-6">
+          <div className="grid gap-2 text-center">
+            <h1 className="text-3xl font-bold">Sign Up</h1>
+            <p className="text-balance text-muted-foreground">
+              Enter your information below to create your account
+            </p>
+          </div>
+          <AuthForm type="sign-up" />
+
+          <div className="mt-4 text-center text-sm">
+            Already have an account?{" "}
+            <Link href="/login" className="underline">
+              Login
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className="hidden bg-muted lg:block">
+        {/*
+
+        <Image
+          src="/login.svg"
+          alt="Image"
+          width="1920"
+          height="1080"
+          className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+        />
+
+          */}
+      </div>
+    </div>
+  );
+}
